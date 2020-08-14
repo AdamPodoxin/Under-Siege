d16afd423705f7c9a4ec510b235c53b7f1f7ad6e
Adam Podoxin <adam.podoxin@gmail.com>
2020-08-13 23:02:05 -0700

    Create basic player movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 4f;
+
+    public bool CanMove { get; set; } = true;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (CanMove)
+        {
+            Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            rb.velocity = movement * moveSpeed;
+        }
+    }
+}
