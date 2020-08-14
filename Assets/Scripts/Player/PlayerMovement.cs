8deae5b83bae43edf6acd2691d375db8d835b50f
Adam Podoxin <adam.podoxin@gmail.com>
2020-08-13 23:21:32 -0700

    Create player movement animation

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,9 +10,15 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D rb;
 
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
@@ -21,6 +27,10 @@ public class PlayerMovement : MonoBehaviour
         {
             Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
             rb.velocity = movement * moveSpeed;
+
+            bool isMoving = movement.x != 0f || movement.y != 0;
+            animator.SetBool("isWalking", isMoving);
+            spriteRenderer.flipX = movement.x < 0f;
         }
     }
 }
