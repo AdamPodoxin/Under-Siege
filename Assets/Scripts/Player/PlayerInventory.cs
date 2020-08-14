ec240c2695d65ec5b673e38652370cd6170ac5a5
Adam Podoxin <adam.podoxin@gmail.com>
2020-08-14 00:37:11 -0700

    Setup player action scripts

--- Assets/Scripts/Player/PlayerInventory.cs
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -4,20 +4,40 @@ using UnityEngine;
 
 public class PlayerInventory : MonoBehaviour
 {
-
+    public PlayerAttack[] playerAttacks;
+    public PlayerAbility[] playerAbilities;
+    public PlayerItem[] playerItems;
 
     public void UseAttack(int index)
     {
+        try
+        {
+            playerAttacks[index].Use();
+        }
+        catch { }
+
         print("Use attack " + index);
     }
 
     public void UseAbility(int index)
     {
+        try
+        {
+            playerAbilities[index].Use();
+        }
+        catch { }
+
         print("Use ability " + index);
     }
 
     public void UseItem(int index)
     {
+        try
+        {
+            playerItems[index].Use();
+        }
+        catch { }
+
         print("Use item " + index);
     }
 }
